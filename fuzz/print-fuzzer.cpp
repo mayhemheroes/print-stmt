050898df575a591ad6855054136c5ec5058a1a91
ebell495 <ericbell495@gmail.com>
2022-09-24 21:59:37 -0700

    Update fuzzing target

--- fuzz/print-fuzzer.cpp
+++ fuzz/print-fuzzer.cpp
@@ -32,54 +32,91 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     clear_file();
     if (Size > 1) {
-        int caseTarget = Data[0] % 4;
+        int caseTarget = Data[0] % 8;
         switch (caseTarget) {
             case 0:
             {
-                char* charInput = (char*) malloc(Size - 1);
-                memcpy(charInput, Data + 1, Size - 1);
-                charInput[Size - 2] = 0;
-                pretty::Print(file) * charInput;
-                free(charInput);
+                char* input = (char*) malloc(Size - 1);
+                memcpy(input, Data + 1, Size - 1);
+                input[Size - 2] = 0;
+                pretty::Print(file) * input;
+                free(input);
                 break;
             }
             case 1:
             {
                 if (Size > 1 + sizeof(int)) {
-                    int* intArrayInput = (int*) malloc((Size - 1));
-                    memcpy(intArrayInput, Data + 1, Size - 1);
-                    pretty::Print(file) * intArrayInput;
-                    free(intArrayInput);
+                    int* input = (int*) malloc((Size - 1));
+                    memcpy(input, Data + 1, Size - 1);
+                    pretty::Print(file) * input;
+                    free(input);
                 }
                 break;
             }
             case 2: 
             {
                 if (Size > 1 + sizeof(float)) {
-                    float* floatInput = (float*) malloc((Size - 1));
-                    memcpy(floatInput, Data + 1, Size - 1);
-                    pretty::Print(file) * floatInput[0];
-                    free(floatInput);
+                    float* input = (float*) malloc((Size - 1));
+                    memcpy(input, Data + 1, Size - 1);
+                    pretty::Print(file) * input[0];
+                    free(input);
                 }
                 break;
             }
             case 3: 
             {
                 if (Size > 1 + sizeof(double)) {
-                    double* doubleInput = (double*) malloc((Size - 1));
-                    memcpy(doubleInput, Data + 1, Size - 1);
-                    pretty::Print(file) * doubleInput[0];
-                    free(doubleInput);
+                    double* input = (double*) malloc((Size - 1));
+                    memcpy(input, Data + 1, Size - 1);
+                    pretty::Print(file) * input[0];
+                    free(input);
                 }
                 break;
             }
             case 4: 
             {
                 if (Size > 1 + sizeof(void*)) {
-                    void* doubleInput = (void*) malloc((Size - 1));
-                    memcpy(doubleInput, Data + 1, Size - 1);
-                    pretty::Print(file) * doubleInput;
-                    free(doubleInput);
+                    void* input = (void*) malloc((Size - 1));
+                    memcpy(input, Data + 1, Size - 1);
+                    pretty::Print(file) * input;
+                    free(input);
+                }
+                break;
+            }
+            case 5: 
+            {
+                if (Size > 1 + sizeof(bool*)) {
+                    bool* input = (bool*) malloc((Size - 1));
+                    memcpy(input, Data + 1, Size - 1);
+                    pretty::Print(file) * input[0];
+                    free(input);
+                }
+                break;
+            }
+            case 6: 
+            {
+                if (Size > 1 + 2 * sizeof(char*)) {
+                    char* input1 = (char*) malloc((Size - 1));
+                    memcpy(input1, Data + 1, (Size - 1) / 2);
+                    char* input2 = (char*) malloc((Size - 1));
+                    memcpy(input2, Data + 1 + ((Size - 1) / 2), (Size - 1) / 2);
+                    pretty::Print(file) * input1, input2;
+                    free(input1);
+                    free(input2);
+                }
+                break;
+            }
+            case 7: 
+            {
+                if (Size > 1 + sizeof(char*) + 4 * sizeof(int)) {
+                    int* input1_a = (int*) malloc(4 * sizeof(int));
+                    int input1[4];
+                    memcpy(input1, input1_a, sizeof(input1));
+                    char* input2 = (char*) malloc((Size - 1 - 4 * sizeof(int)));
+                    memcpy(input2, Data + 1 + 4 * sizeof(int), (Size - 1 - 4 * sizeof(int)));
+                    pretty::Print(file) * input1, input2;
+                    free(input1_a);
+                    free(input2);
                 }
                 break;
             }
